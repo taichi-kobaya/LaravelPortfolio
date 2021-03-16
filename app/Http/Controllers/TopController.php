03c6ab78ccd8b0148ba4500abc9ca0386937ae9c
taichi-kobaya <taichi.kobayashi@eeeeg.tokyo>
2021-03-16 22:46:30 +0900

    ルーティング→コントローラ→ビューの流れ

---
+++ app/Http/Controllers/TopController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TopController extends Controller
+{
+    public function allUsers()
+   {
+       return view("taichihome/index");
+   }
+}
+
