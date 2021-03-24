d1cad06fd5bb338df3251f04a478b32f0cb27995
taichi-kobaya <taichi.kobayashi@eeeeg.tokyo>
2021-03-25 02:14:03 +0900

    マイグレーションファイルを作成する #7 contactテーブルを作成し、マイグレーション実行しました。

---
+++ database/migrations/2021_03_25_014659_create_contact_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contact', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->text('message')->comment('メッセージ');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contact');
+    }
+}
