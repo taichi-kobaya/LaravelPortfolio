9a28fb8749b15cc2134c2ed556989a0111fac59c
taichi-kobaya <taichi.kobayashi@eeeeg.tokyo>
2021-03-26 22:55:31 +0900

    Blade ファイルの文法にする #6 photoswipe.blade.php,script.blade.phpを作成、views/layouts配下に部品化して配置しました。

---
+++ resources/views/layouts/script.blade.php
@@ -0,0 +1,6 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script type='text/javascript' src="js/photoswipe/photoswipe.min.js"></script>
+  <script type='text/javascript' src="js/photoswipe/photoswipe-ui-default.js"></script>
+  <script type='text/javascript' src="js/photoswipe/photoswipe-setup.js"></script>
+  <script type='text/javascript' src="js/lazysizes/lazysizes.min.js"></script>
+  <script type='text/javascript' src="js/script.js"></script>
